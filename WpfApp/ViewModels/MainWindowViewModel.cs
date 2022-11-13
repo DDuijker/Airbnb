988b163624cb67e5fbab8f633d97219d6923b515
DDuijker <djoekeduijker@live.nl>
2022-11-13 17:42:27 +0100

    Added folders and changed layout

---
+++ WpfApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using AirBnB.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp;
+
+namespace AirBnB.ViewModel
+{
+    public class MainWindowViewModel
+    {
+
+        public void LandlordClick()
+        {
+            ///redirect to landlord view
+        }
+
+    }
+}
--- WpfApp/Views/Landlords.xaml.cs
+++ WpfApp/Views/Landlords.xaml.cs
@@ -10,19 +10,23 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp
+namespace AirBnB.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Landlords.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Landlords : Window
     {
-        public MainWindow()
+        public Landlords()
         {
             InitializeComponent();
+        }
+
+        private void EditLandlordButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
